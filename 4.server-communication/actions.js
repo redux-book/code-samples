b8a838cc1631934e63dfdc3f8a5101ab6c55e77d
Boris Dinkevich <your_email@example.com>
2017-04-09 12:19:44 -0400

    Use local json file instead of real network access

--- 4.server-communication/actions.js
+++ 4.server-communication/actions.js
@@ -1,7 +1,7 @@
 const fetchRecipes = () => ({
   type: API_REQUEST,
   payload: {
-    url: 'https://s3.amazonaws.com/500tech-shared/db.json',
+    url: 'db.json',
     next: FETCH_RECIPES
   }
 });
